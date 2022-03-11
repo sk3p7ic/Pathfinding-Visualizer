e122f59d50335f3f88a0ae9a73fd127aaa00eaa7
Joshua Ibrom <joshuaibrom@gmail.com>
2022-03-11 17:11:01 -0600

    Boilerplate for pathfinding-grid

---
+++ src/app/pathfinding-visualizer/pathfinding-grid/pathfinding-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pathfinding-grid',
+  templateUrl: './pathfinding-grid.component.html',
+  styleUrls: ['./pathfinding-grid.component.sass']
+})
+export class PathfindingGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pathfinding-visualizer/pathfinding-visualizer.module.ts
+++ src/app/pathfinding-visualizer/pathfinding-visualizer.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PathfindingVisualizerComponent } from './pathfinding-visualizer.component';
+import { PathfindingGridComponent } from './pathfinding-grid/pathfinding-grid.component';
 
 
 
 @NgModule({
   declarations: [
-    PathfindingVisualizerComponent
+    PathfindingVisualizerComponent,
+    PathfindingGridComponent
   ],
   imports: [
     CommonModule
